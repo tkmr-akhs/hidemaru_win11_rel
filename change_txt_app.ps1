a8ae5d34aab52fb48ac6df9f3efb37e8be241a48
TAKAMURA Akihisa <tkmr.akhs@gmail.com>
2024-06-13 15:52:24 +0900

    変更
    キー名(txt_hidemaru)を変数にした。

--- change_txt_app.ps1
+++ change_txt_app.ps1
@@ -1,17 +1,18 @@
 ﻿$hidemaru = "D:\Program Files\Hidemaru\Hidemaru.exe"
 $file_icon = "%SystemRoot%\System32\SHELL32.dll,70"
 $exts = @(".txt", ".log")
+$key_name = "txt_hidemaru"
 
-New-Item -Path HKCU:\SOFTWARE\Classes\txt_hidemaru
-New-Item -Path HKCU:\SOFTWARE\Classes\txt_hidemaru\DefaultIcon
-New-Item -Path HKCU:\SOFTWARE\Classes\txt_hidemaru\shell
-New-Item -Path HKCU:\SOFTWARE\Classes\txt_hidemaru\shell\open
-New-Item -Path HKCU:\SOFTWARE\Classes\txt_hidemaru\shell\open\command
+New-Item -Path "HKCU:\SOFTWARE\Classes\$($key_name)" | Out-Null
+New-Item -Path "HKCU:\SOFTWARE\Classes\$($key_name)\DefaultIcon" | Out-Null
+New-Item -Path "HKCU:\SOFTWARE\Classes\$($key_name)\shell" | Out-Null
+New-Item -Path "HKCU:\SOFTWARE\Classes\$($key_name)\shell\open" | Out-Null
+New-Item -Path "HKCU:\SOFTWARE\Classes\$($key_name)\shell\open\command" | Out-Null
 
 Set-ItemProperty HKCU:\SOFTWARE\Classes\txt_hidemaru\DefaultIcon -Name "(default)" -Value $file_icon
 Set-ItemProperty HKCU:\SOFTWARE\Classes\txt_hidemaru\shell\open\command -Name "(default)" -Value $hidemaru
 
 foreach ($ext in $exts) {
     Get-ItemProperty HKCU:\SOFTWARE\Classes\$ext
-    Set-ItemProperty HKCU:\SOFTWARE\Classes\$ext -Name "(default)" -Value "txt_hidemaru"
+    Set-ItemProperty HKCU:\SOFTWARE\Classes\$ext -Name "(default)" -Value $key_name
 }
