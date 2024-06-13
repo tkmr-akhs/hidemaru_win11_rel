fbf822e20f8e2fa9cbd6799ff598df132481e9b0
TAKAMURA Akihisa <tkmr.akhs@gmail.com>
2024-06-13 15:30:10 +0900

    Initial Commit
    Initial commit.

---
+++ change_txt_app.ps1
@@ -0,0 +1,17 @@
+﻿$hidemaru = "D:\Program Files\Hidemaru\Hidemaru.exe"
+$file_icon = "%SystemRoot%\System32\SHELL32.dll,70"
+$exts = @(".txt", ".log")
+
+New-Item -Path HKCU:\SOFTWARE\Classes\txt_hidemaru
+New-Item -Path HKCU:\SOFTWARE\Classes\txt_hidemaru\DefaultIcon
+New-Item -Path HKCU:\SOFTWARE\Classes\txt_hidemaru\shell
+New-Item -Path HKCU:\SOFTWARE\Classes\txt_hidemaru\shell\open
+New-Item -Path HKCU:\SOFTWARE\Classes\txt_hidemaru\shell\open\command
+
+Set-ItemProperty HKCU:\SOFTWARE\Classes\txt_hidemaru\DefaultIcon -Name "(default)" -Value $file_icon
+Set-ItemProperty HKCU:\SOFTWARE\Classes\txt_hidemaru\shell\open\command -Name "(default)" -Value $hidemaru
+
+foreach ($ext in $exts) {
+    Get-ItemProperty HKCU:\SOFTWARE\Classes\$ext
+    Set-ItemProperty HKCU:\SOFTWARE\Classes\$ext -Name "(default)" -Value "txt_hidemaru"
+}
