825b90bbe322476cc508a130f7fcf522613be416
TAKAMURA Akihisa <tkmr.akhs@gmail.com>
2024-10-16 14:00:43 +0900

    機能追加
    編集および新規にも対応した。

--- change_txt_app.ps1
+++ change_txt_app.ps1
@@ -1,7 +1,9 @@
-﻿$editor = '"D:\Program Files\Hidemaru\Hidemaru.exe" "%1"'
+﻿$editor_rw = '"D:\Program Files\Hidemaru\Hidemaru.exe" "%1"'
+$editor_ro = '"D:\Program Files\Hidemaru\Hidemaru.exe" /r "%1"'
 $file_icon = "%SystemRoot%\System32\SHELL32.dll,70"
 $exts = @(".txt", ".log")
 $key_name = "txt_hidemaru"
+$opennew_title = "新規(&N)"
 
 $reg_key = "HKCU:\SOFTWARE\Classes\$($key_name)"
 try { New-Item -Path $reg_key -ErrorAction Stop | Out-Null }
@@ -23,8 +25,27 @@ $reg_key = "HKCU:\SOFTWARE\Classes\$($key_name)\shell\open\command"
 try { New-Item -Path $reg_key -ErrorAction Stop | Out-Null }
 catch { Write-Debug "The registory key '$reg_key' has already exist." }
 
+$reg_key = "HKCU:\SOFTWARE\Classes\$($key_name)\shell\opennew"
+try { New-Item -Path $reg_key -ErrorAction Stop | Out-Null }
+catch { Write-Debug "The registory key '$reg_key' has already exist." }
+
+$reg_key = "HKCU:\SOFTWARE\Classes\$($key_name)\shell\opennew\command"
+try { New-Item -Path $reg_key -ErrorAction Stop | Out-Null }
+catch { Write-Debug "The registory key '$reg_key' has already exist." }
+
+$reg_key = "HKCU:\SOFTWARE\Classes\$($key_name)\shell\Edit"
+try { New-Item -Path $reg_key -ErrorAction Stop | Out-Null }
+catch { Write-Debug "The registory key '$reg_key' has already exist." }
+
+$reg_key = "HKCU:\SOFTWARE\Classes\$($key_name)\shell\Edit\command"
+try { New-Item -Path $reg_key -ErrorAction Stop | Out-Null }
+catch { Write-Debug "The registory key '$reg_key' has already exist." }
+
 Set-ItemProperty HKCU:\SOFTWARE\Classes\txt_hidemaru\DefaultIcon -Name "(default)" -Value $file_icon
-Set-ItemProperty HKCU:\SOFTWARE\Classes\txt_hidemaru\shell\open\command -Name "(default)" -Value $editor
+Set-ItemProperty HKCU:\SOFTWARE\Classes\txt_hidemaru\shell\open\command -Name "(default)" -Value $editor_rw
+Set-ItemProperty HKCU:\SOFTWARE\Classes\txt_hidemaru\shell\opennew -Name "(default)" -Value $opennew_title
+Set-ItemProperty HKCU:\SOFTWARE\Classes\txt_hidemaru\shell\opennew\command -Name "(default)" -Value $editor_ro
+Set-ItemProperty HKCU:\SOFTWARE\Classes\txt_hidemaru\shell\Edit\command -Name "(default)" -Value $editor_rw
 
 foreach ($ext in $exts) {
     Get-ItemProperty HKCU:\SOFTWARE\Classes\$ext
